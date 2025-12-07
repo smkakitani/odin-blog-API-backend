89f71658c7ffd83af5ea58cc968ef0fce516f78f
smkakitani <82690252+kakisaura@users.noreply.github.com>
2025-12-06 22:57:38 -0300

    add punctuation

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -23,7 +23,7 @@ indexRouter.get("/", (req, res) => res.send("Hello! From Blog API :3"));
 
 // Sign up & log in/out
 indexRouter.post("/sign-up/author", indexController.authorSignUp);
-indexRouter.post("/sign-up", indexController.visitorSignUp)
+indexRouter.post("/sign-up", indexController.visitorSignUp);
 indexRouter.post("/log-in", indexController.logIn);
 // When the user logs out, you can have the client remove the JWT from localStorage.
 indexRouter.get("/log-out", indexController.logOut);
