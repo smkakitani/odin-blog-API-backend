b82f3bff52691d45d7b24590dd210414fd273d44
smkakitani <82690252+kakisaura@users.noreply.github.com>
2026-01-04 21:19:30 -0300

    clean up code

--- src/app.js
+++ src/app.js
@@ -4,15 +4,6 @@ require("dotenv").config();
 // Server
 const express = require("express");
 const cors = require("cors");
-// const session = require("express-session");
-// const passport = require("passport");
-// const passport = require("./config/passport");
-const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
-const jwtStrategy = require("./config/passport");
-
-// Config imports
-// const prisma = require("./config/database");
-// require("./config/passport")(passport); // Pass the global passport object into the configuration function
 
 // Import routers
 const indexRouter = require("./routes/indexRouter");
@@ -22,51 +13,26 @@ const app = express();
 
 
 // Cross-Origin Resource Sharing
-app.use(cors());
-/* app.use(cors({
+app.use(cors({
   methods: ['GET', 'POST', 'DELETE', 'PUT'],
-})); */
+  origin: process.env.LOCAL_HOST || process.env.URL_HOST, // Vite's port
+}));
 
 // Enable req.body to parse client's output
-// app.use(express.json()) // For parsing application/json
+app.use(express.json()) // For parsing application/json
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
-// Prisma session store
-/* app.use(session({
-  cookie: {
-    maxAge: 7 * 24 * 60 * 60 * 1000 // ms
-  },
-  secret: process.env.SECRET_SESSION,
-  resave: true,
-  saveUninitialized: true,
-  store: new PrismaSessionStore(
-    prisma, {
-      checkPeriod: 2 * 60 * 1000, // ms
-      dbRecordIdIsSessionId: true,
-      dbRecordIdFunction: undefined,
-    })
-})); */
-
-// Passport auth
-// app.use(passport.session());
-// app.use(passport.)
-// passport.use(jwtStrategy);
 
 // Custom middleware to access current user
-app.use((req, res, next) => {
-  // res.currentUser = req.user;
+// app.use((req, res, next) => {
 
-  // console.log(req.session);
-  // console.log('from main: ',req.logout);
-  // console.log(req.body);
-  next();
-});
+//   // console.log(req.originalUrl);
+//   next();
+// });
 
 
 
 // Routers
-// app.use("/api", indexRouter);
-// app.use("/api/author", authorRouter);
 app.use("/", indexRouter);
 
 
@@ -76,5 +42,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
   if (err) throw err;
 
-  console.log(`Blog API - listening on port ${PORT} :3`);
+  // console.log(`Blog API - listening on port ${PORT} :3`);
 });
