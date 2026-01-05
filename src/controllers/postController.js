0e61aa46e7a02fc0426086bd80c02ce34e26f001
smkakitani <82690252+kakisaura@users.noreply.github.com>
2026-01-04 23:09:19 -0300

    add authentication to routes, rename controller, modify function to accept query, allPosts returning: published posts, sorting by ascending and include author's name. Add published boolean when creating post

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -2,32 +2,90 @@
 const prisma = require("../config/database");
 const { Prisma } = require("../../generated/prisma");
 const { 
+  validateQuery,
   validatePost, 
   validatePostParams, 
   validateAuthorParams, 
   validationResult, 
-  matchedData 
+  matchedData,   
 } = require("../utils/validation");
 
 
 
 // 
-async function postAll(req, res, next) {
-  try {
-    // If post doesn't exist, it'll return an empty array
+const posts = [
+  validateQuery, // Check for request's query
+  async (req, res, next) => {
+    // Check if request for posts have any query
+    if (Object.keys(req.query).length > 0) {
+      return next();
+    }
+
+    // Request to return all posts - empty array if no posts exist
     const allPosts = await prisma.post.findMany({
+      where: {
+        published: true,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
       include: {
         _count: {
           select: { comments: true }
+        },
+        author: {
+          select: {
+            firstName: true,
+            lastName: true,
+          }
         }
       }
     });
 
     res.json(allPosts);
-  } catch (err) {
-    next(err);
-  }  
-}
+  }, async (req, res, next) => {
+    // TODO: query/filter
+    const query = req.query;
+    const errors = validationResult(req);
+    const { id } = matchedData(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        status: 400,
+        errMsg: "Invalid query.",
+        err: errors.array(),
+      });
+    }
+
+    const post = await prisma.post.findUnique({
+      where: {
+        id: id,
+      }
+    });
+    // if (sort === "created") {
+    //   console.log("sorting posts by createdAt...");
+
+    //   const postsByDate = await prisma.post.findMany({
+    //     // where: {
+    //     //   published: true
+    //     // },
+    //     orderBy: [
+    //       { createdAt: "asc" }
+    //     ]
+    //     ,include: {
+    //       _count: {
+    //         select: { comments: true }
+    //       }
+    //     }
+    //   });
+
+    //   return res.json(postsByDate);
+    // }
+
+    // console.log(query.id);
+    res.json(post);
+  }
+];
 
 const postByAuthorId = [
   validateAuthorParams,
@@ -88,6 +146,7 @@ const postCreate = [
       // Using TinyMCE to populate content - 
       const author = req.user;
       const errors = validationResult(req);
+      // console.log("calling postCreate!");
 
       if (!errors.isEmpty()) {
         return res.status(400).json({
@@ -116,13 +175,14 @@ const postCreate = [
       }
 
       // 
-      const { title, content } = matchedData(req);
+      const { title, content, published } = matchedData(req);
 
       const newPost = await prisma.post.create({
         data: {
           title,
           content,
-          authorId: author.id
+          published: (published === "yes") || Prisma.skip,
+          authorId: author.id,
         },
         include: {
           author: {
@@ -136,7 +196,7 @@ const postCreate = [
         }
       });
       
-      res.status(201).json(newPost);
+      res.status(201).json(newPost /* {post: {title,content, published}} */);
     } catch (err) {
       next(err);
     }
@@ -151,6 +211,7 @@ const postUpdate = [
     try {
       const author = req.user;
       const errors = validationResult(req);
+      // console.log(author);
 
       if (!errors.isEmpty() && (errors.mapped()?.authorId || errors.mapped()?.postId)) {
         // Filtering errors of parameters field
@@ -173,6 +234,7 @@ const postUpdate = [
 
       // Check author & post
       const { postId, authorId } = matchedData(req, { locations: ["params"] });
+      console.log(typeof postId,typeof authorId)
       const isAuthor = await prisma.author.findUnique({
         where: { id: authorId }
       });
@@ -193,28 +255,29 @@ const postUpdate = [
       }
 
       // 
-      const { title, content, published } = matchedData(req, { locations: ["body"] });
+      // const { title, content, published } = matchedData(req, { locations: ["body"] });
 
-      const updatedPost = await prisma.post.update({
-        where: { 
-          authorId: {
-            equals: author.id,
-          },
-          id: postId
-        },
-        data: {
-          title,
-          content,
-          published: (published === "yes") || Prisma.skip,
-        },
-        include: {
-          _count: {
-            select: { comments: true },
-          }
-        }
-      });
+      // const updatedPost = await prisma.post.update({
+      //   where: { 
+      //     authorId: {
+      //       equals: authorId, // change back to author.id
+      //     },
+      //     id: postId
+      //   },
+      //   data: {
+      //     title,
+      //     content,
+      //     published: (published === "yes") || Prisma.skip,
+      //   },
+      //   include: {
+      //     _count: {
+      //       select: { comments: true },
+      //     }
+      //   }
+      // });
 
-      res.json({ status: 200, post: updatedPost });
+      // res.json({ status: 200, post: updatedPost });
+      res.json({ msg: "huh updating post..."});
     } catch (err) {
       next(err);
     }
@@ -273,7 +336,7 @@ const postDelete = [
 
 
 module.exports = {
-  postAll,
+  posts,
   postByAuthorId,
   postCreate,
   postUpdate,
--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -1,23 +1,26 @@
 const { Router } = require("express");
 const postController = require("../controllers/postController");
 
+// Authentication
+const { auth } = require("../config/passport");
+
 const postRouter = Router();
 
 
 
 // Routers - /posts
 // Read posts
-postRouter.get("/", postController.postAll);
-postRouter.get("/:authorId", postController.postByAuthorId);
+postRouter.get("/", postController.posts);
+postRouter.get("/:authorId", auth, postController.postByAuthorId);
 
 // Create posts - every post must have an author
-postRouter.post("/", postController.postCreate);
+postRouter.post("/", auth, postController.postCreate);
 
 // Update posts - 
-postRouter.put("/:authorId/:postId", postController.postUpdate);
+postRouter.put("/:authorId/:postId", auth, postController.postUpdate);
 
 // Delete posts - deleting, in fact, is setting publish to false
-postRouter.delete("/:postId", postController.postDelete);
+postRouter.delete("/:postId", auth, postController.postDelete);
 
 
 
