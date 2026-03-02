9204c21d3b801c857d30f1c01e23f312ff40f607
smkakitani <82690252+kakisaura@users.noreply.github.com>
2026-03-02 17:26:46 -0300

    add authentication to routers

--- src/routes/authorRouter.js
+++ src/routes/authorRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
-const authorController = require("../controllers/authorController")
+const authorController = require("../controllers/authorController");
+const { auth } = require("../config/passport");
 
 const authorRouter = Router();
 
@@ -8,15 +9,15 @@ const authorRouter = Router();
 // Routers - /authors
 // Read authors
 authorRouter.get("/", authorController.authorAll);
-authorRouter.get("/:authorId", authorController.authorById);
+authorRouter.get("/:authorId", auth, authorController.authorById);
 
 // Create/Sign up author ---> indexRouter
 
 // Update author
-authorRouter.put("/:authorId", authorController.authorEdit);
+authorRouter.put("/:authorId", auth, authorController.authorEdit);
 
 // Delete author... should not remove the only author
-authorRouter.delete(["/:authorId", "/:authorEmail"], authorController.authorDelete);
+authorRouter.delete(["/:authorId", "/:authorEmail"], auth, authorController.authorDelete);
 
 
 
